133456f240672bb210fd1220decde220dfef4473
Alisher <sherlock.homes.0522@gmail.com>
2023-04-24 10:30:23 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let a = '15';
+console.log(typeof(a));
+
+console.log(1);
+
+const obj = {
+    name: "Alisher",
+    age: 25
+};
+
+console.log(obj, typeof(obj), typeof(obj.name));
